4b5e7e3f3184b03113e1dc5d33d72507ab750427
marcgardiner <marcgardinerdriven@gmail.com>
2018-08-22 02:15:48 +1000

    add SPA route

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 var bodyParser = require('body-parser');
 require('./app_api/models/db');
 
+var routes = require('./app_server/routes/index');
 var routesApi = require('./app_api/routes/index');
 
 var app = express();
@@ -19,7 +20,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'app_client')));
 
+app.use('/', routes);
 app.use('/api', routesApi);
 
 // catch 404 and forward to error handler
