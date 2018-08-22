76dde0f24a0560c4ecaef5928ab225eb59a47131
marcgardiner <marcgardinerdriven@gmail.com>
2018-08-23 00:58:12 +1000

    adding routes for spa

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var routesApi = require('./app_api/routes/index');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'app_server/views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
--- app_client/app.js
+++ app_client/app.js
@@ -1,2 +1,14 @@
-angular.module('zispaApp', []);
+angular.module('zispaApp', ['ngRoute']);
 
+function config($routeProvider) {
+    $routeProvider
+    .when('/', {
+        templateUrl: 'home/home.view.html',
+        controller: 'homeCtrl'
+    })
+    .otherwise({redirectTo: '/'});
+}
+
+angular
+.module('zispaApp')
+.config(['$routeProvider', config]);
