d6ab31ea88a09f096a63af81718a632b049a7f4b
marcgardiner <marcgardinerdriven@gmail.com>
2018-08-26 03:11:44 +1000

    add filter linebreaks in a string

---
+++ app_client/common/filters/addHtmlLineBreaks.filter.js
@@ -0,0 +1,12 @@
+(function () {
+    angular
+        .module('zispaApp')
+        .filter('addHtmlLineBreaks', addHtmlLineBreaks);
+    
+    function addHtmlLineBreaks () {
+        return function (text) {
+            var output = text.replace(/\n/g, '<br/>');
+            return output;
+        };
+    }
+})();
