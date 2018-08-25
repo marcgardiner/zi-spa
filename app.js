bc7a7d95b3ae727cd3c3fa7614673eba5f230a30
marcgardiner <marcgardinerdriven@gmail.com>
2018-08-26 03:06:20 +1000

    add page about

--- app.js
+++ app.js
@@ -26,7 +26,8 @@ var appClientFiles = [
   'app_client/common/directives/ratingStars/ratingStars.directive.js',
   'app_client/common/directives/footerGeneric/footerGeneric.directive.js',
   'app_client/common/directives/navigation/navigation.directive.js',
-  'app_client/common/directives/pageHeader/pageHeader.directive.js'
+  'app_client/common/directives/pageHeader/pageHeader.directive.js',
+  'app_client/about/about.controller.js'
 ];
 
 var fileContent = appClientFiles.map(function(file) {
--- app_client/app.js
+++ app_client/app.js
@@ -8,6 +8,11 @@
                 controller: 'homeCtrl',
                 controllerAs: 'vm'
             })
+            .when('/about', {
+                templateUrl: '/common/views/genericText.view.html',
+                controller: 'aboutCtrl',
+                controllerAs: 'vm'
+            })
             .otherwise({redirectTo: '/'});
         $locationProvider.html5Mode(true);
     }
