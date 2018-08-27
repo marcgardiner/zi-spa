432fb11cefe67feda5215f38547e8a7b7422fb47
marcgardiner <marcgardinerdriven@gmail.com>
2018-08-27 11:57:07 +1000

    add integration with location detail service

--- app_client/locationDetail/locationDetail.controller.js
+++ app_client/locationDetail/locationDetail.controller.js
@@ -3,13 +3,24 @@
         .module('zispaApp')
         .controller('locationDetailCtrl', locationDetailCtrl);
 
-    locationDetailCtrl.$inject = ['$routeParams'];
-    function locationDetailCtrl () {
+    locationDetailCtrl.$inject = ['$routeParams', 'zispaData'];
+    function locationDetailCtrl ($routeParams, zispaData) {
         var vm = this;
 
         vm.locationid = $routeParams.locationid;
         vm.pageHeader = {
             title: vm.locationid
         };
+
+        zispaData.locationById(vm.locationid)
+            .success(function (data) {
+                vm.data = {location: data};
+                vm.pageHeader = {
+                    title: vm.data.location.name
+                }
+            })
+            .error(function (e) {
+                console.log(e);
+            });
     }
 })();
