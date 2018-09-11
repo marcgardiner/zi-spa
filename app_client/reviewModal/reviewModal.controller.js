f986dc242b4e2f3e8a91b2f30d66ec8bcc493a28
marcgardiner <marcgardinerdriven@gmail.com>
2018-09-12 04:13:45 +1000

    update modal controller

--- app_client/reviewModal/reviewModal.controller.js
+++ app_client/reviewModal/reviewModal.controller.js
@@ -3,8 +3,8 @@
         .module('zispaApp')
         .controller('reviewModalCtrl', reviewModalCtrl);
 
-    reviewModalCtrl.$inject = ['$modalInstance', 'locationData'];
-    function reviewModalCtrl ($modalInstance, locationData) {
+    reviewModalCtrl.$inject = ['$modalInstance', 'locationData', 'zispaData'];
+    function reviewModalCtrl ($modalInstance, locationData, zispaData) {
         var vm = this;
 
         vm.modal = {
@@ -13,5 +13,30 @@
             }
         };
         vm.locationData = locationData;
+
+        vm.onSubmit = function () {
+            vm.formError = "";
+            if (!vm.formData.name || !vm.formData.rating || !vm.formData.reviewText) {
+                vm.formError = "All fields required, please try again";
+                return false;
+            } else {
+                vm.doAddReview(vm.locationData.locationid, vm.formData);
+            }        
+        };
+
+        vm.doAddReview = function (locationid, formData) {
+            zispaData.addReviewById(locationid, {
+                author: formData.name,
+                rating: formData.rating,
+                reviewText: formData.rating
+            })
+                .success(function (data) {
+                    console.log("Success!");
+                })
+                .error(function (data) {
+                    vm.formError = "Your review has not been saved, try again!";
+                });
+            return false;
+        };
     }
 })();
