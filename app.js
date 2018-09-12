5cdb986e30b4dbe6c502515f690e1d462d5673ad
marcgardiner <marcgardinerdriven@gmail.com>
2018-09-12 20:42:31 +1000

    lint; fix server side

--- app.js
+++ app.js
@@ -1,50 +1,53 @@
 require('dotenv');
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var bodyParser = require('body-parser');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
 require('./app_api/models/db');
-var uglifyJs = require("uglify-js");
-var fs = require('fs');
+const uglifyJs = require('uglify-js');
+const fs = require('fs');
 
-var routesApi = require('./app_api/routes/index');
+const routesApi = require('./app_api/routes/index');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'jade');
 
 // uglify
-var appClientFiles = [
-  'app_client/app.js',
-  'app_client/home/home.controller.js',
-  'app_client/about/about.controller.js',
-  'app_client/common/services/geolocation.service.js',
-  'app_client/common/services/zispaData.service.js',
-  'app_client/common/filters/formatDistance.filter.js',
-  'app_client/common/filters/addHtmlLineBreaks.filter.js',
-  'app_client/common/directives/ratingStars/ratingStars.directive.js',
-  'app_client/common/directives/footerGeneric/footerGeneric.directive.js',
-  'app_client/common/directives/navigation/navigation.directive.js',
-  'app_client/common/directives/pageHeader/pageHeader.directive.js',
-  'app_client/locationDetail/locationDetail.controller.js'
+const appClientFiles = [
+    'app_client/app.js',
+    'app_client/home/home.controller.js',
+    'app_client/about/about.controller.js',
+    'app_client/common/services/geolocation.service.js',
+    'app_client/common/services/zispaData.service.js',
+    'app_client/common/filters/formatDistance.filter.js',
+    'app_client/common/filters/addHtmlLineBreaks.filter.js',
+    'app_client/common/directives/ratingStars/ratingStars.directive.js',
+    'app_client/common/directives/footerGeneric/footerGeneric.directive.js',
+    'app_client/common/directives/navigation/navigation.directive.js',
+    'app_client/common/directives/pageHeader/pageHeader.directive.js',
+    'app_client/locationDetail/locationDetail.controller.js'
 ];
 
-var fileContent = appClientFiles.map(function(file) {
-  return fs.readFileSync(file, 'utf-8');
+const fileContent = appClientFiles.map((file) => {
+    return fs.readFileSync(file, 'utf-8');
 });
 
-var uglified = uglifyJs.minify(fileContent, { compress: false });
+const uglified = uglifyJs.minify(fileContent, {
+    compress: false
+});
 
-fs.writeFile('public/angular/zispa.min.js', uglified.code, function (err) {
-  if(err) {
-    console.log(err);
-  } else {
-    console.log('Script generated and saved: zispa.min.js');
-  }
+fs.writeFile('public/angular/zispa.min.js', uglified.code, (err) => {
+    if (err) {
+        console.log(err);
+    }
+    else {
+        console.log('Script generated and saved: zispa.min.js');
+    }
 });
 
 app.use(logger('dev'));
@@ -56,34 +59,33 @@ app.use(express.static(path.join(__dirname, 'app_client')));
 
 app.use('/api', routesApi);
 
-app.use(function (req, res) {
-  res.sendfile(path.json(__dirname, 'app_client', 'index.html'));
+app.use((req, res) => {
+    res.sendfile(path.json(__dirname, 'app_client', 'index.html'));
 });
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use((req, res, next) => {
+    let err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-      res.status(err.status || 500);
-      res.render('error', {
-          message: err.message,
-          error: err
-      });
-  });
+    app.use((err, req, res, next) => {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
 }
 
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-      message: err.message,
-      error: {}
-  });
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
-
 
 module.exports = app;
