05719e2fdc84effd382967129f9196ba0b37bbe9
marcgardiner <marcgardinerdriven@gmail.com>
2018-09-14 16:56:52 +1000

    add router for auth

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const ctrlLocations = require('../controllers/locations');
 const ctrlReviews = require('../controllers/reviews');
+const ctrlAuth = require('../controllers/authentication');
 
 router.get('/locations', ctrlLocations.locationsListByDistance);
 router.post('/locations', ctrlLocations.locationsCreate);
@@ -14,5 +15,9 @@ router.post('/locations/:locationid/reviews', ctrlReviews.reviewsCreate);
 router.get('/locations/:locationid/reviews/:reviewid', ctrlReviews.reviewsReadOne);
 router.put('/locations/:locationid/reviews/:reviewid', ctrlReviews.reviewsUpdateOne);
 router.delete('/locations/:locationid/reviews/:reviewid', ctrlReviews.reviewsDeleteOne);
+
+// auth
+router.post('/register', ctrlAuth.register);
+router.post('/login', ctrlAuth.login);
 
 module.exports = router;
