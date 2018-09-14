c6a0472a74eebb82a353872c303b3924a2527518
marcgardiner <marcgardinerdriven@gmail.com>
2018-09-14 20:02:48 +1000

    FIX; remove typos

--- app_api/models/users.js
+++ app_api/models/users.js
@@ -34,7 +34,7 @@ userSchema.methods.generateJWT = () => {
         _id: this._id,
         email: this.email,
         name: this.name,
-        exp: parseInt(expiry.getTime() / 1000),
+        exp: parseInt(expiry.getTime() / 1000)
     }, process.env.JWT_SECRET);
 };
 
